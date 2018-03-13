d2ca975882b3d4e1e4ce7a3780b0c8d453c2f289
Morgan Holland <morgan.holland@gmail.com>
2018-03-13 08:19:03 -0700

    added ability to create new ToDo item in list

--- src/App.js
+++ src/App.js
@@ -11,17 +11,41 @@ class App extends Component {
         { description: 'Walk the cat', isCompleted: true },
         { description: 'Throw the dishes away', isCompleted: false },
         { description: 'Buy new dishes', isCompleted: false }
-      ]
+      ],
+      newToDoDescription: ''
     };
   }
+
+ handleChange(e) {
+   this.setState({ newToDoDescription: e.target.value })
+ }
+
+ handleSubmit(e) {
+   e.preventDefault();
+   if (!this.state.newToDoDescription) {return}
+   const newToDo = { description: this.state.newToDoDescription, isCompleted: false };
+   this.setState({ todos: [...this.state.todos, newToDo], newToDoDescription: '' });
+ }
+
+ toggleComplete(index) {
+   const todos = this.state.todos.slice();
+   const todo = todos[index];
+   todo.isCompleted = todo.isCompleted ? false : true;
+   this.setState({ todos: todos });
+ }
+
   render() {
     return (
       <div className="App">
-       <ul>
-        { this.state.todos.map( (todo, index) =>
-        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted }/>
-      )}
-      </ul>
+         <ul>
+          { this.state.todos.map( (todo, index) =>
+          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+        )}
+        </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+         <input type="text" value={ this.state.newToDoDescription } onChange={ (e) => this.handleChange(e) }/>
+         <input type="submit" />
+        </form>
       </div>
     );
   }
